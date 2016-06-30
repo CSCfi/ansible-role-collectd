f42ede043c90a308813a332f2a66be75c5f469c4
Johan Guldmyr <johan.guldmyr@csc.fi>
2016-06-30 10:22:36 +0300

    remove old travis test files and print collectd.conf

--- tests/launch-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build --rm -t local/c7-systemd -f tests/Dockerfile .
-sudo docker run -it --privileged=true -e "container=docker" --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/ansible-role-collectd:rw local/c7-systemd /bin/bash -c "/ansible-role-collectd/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
